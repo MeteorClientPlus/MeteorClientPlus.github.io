69b9bfa1deff66e5e67a52523b8762404dc59ba3
Neki_play <neci.03@mail.ru>
2024-09-19 08:27:29 +0500

    Init

---
+++ assets/js/github-api.js
@@ -0,0 +1,37 @@
+var apiRoot = "https://api.github.com/";
+
+window.onload = function(e){ 
+    console.log(window.location.pathname)
+    if ( window.location.pathname.match("index") != null || window.location.pathname.match("/MeteorPlus") || window.location.pathname == "/" || window.location.pathname == "" || window.location.pathname.match("MeteorPlus")) {
+        var total = 0;
+        fetch(apiRoot + "repos/" + "MeteorClientPlus" + "/" + "MeteorPlus" + "/releases", {
+            method: "GET"
+        })
+        .then(x => x.json())
+        .then(data => 
+        {
+            var json = data
+            for (var info of json) 
+            {
+                var assets = info["assets"]
+                for (var asset of assets) 
+                {
+                    total += asset.download_count;
+                }
+            }
+			document.getElementById("downloads").innerText = total;
+        })
+    
+        // Stars
+        fetch(apiRoot + "repos/" + "MeteorClientPlus" + "/" + "MeteorPlus", {
+            method: "GET"
+        })
+        .then(x => x.json())
+        .then(data => 
+        {
+            var json = data;
+            document.getElementById("stars").innerText = json["stargazers_count"]
+        })
+		// N
+    } 
+}
